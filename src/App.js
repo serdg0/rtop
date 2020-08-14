9670bff1d5b9b04cf8f9a505394b0105a15bc694
Sergio Diaz <sergio.diazg92@gmail.com>
2020-08-14 12:23:29 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    axios.get('https://api.adminsite.appsinti.com/menu/product')
+      .then(res => {
+        
+      })
+  }, )
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
